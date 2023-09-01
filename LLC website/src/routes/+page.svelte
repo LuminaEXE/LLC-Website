357012bcb953b9a2408dd3dffe70ff6e9923112c
Lumina 3 <86614978+LuminaEXE@users.noreply.github.com>
2023-09-01 18:37:47 +0000

    Added routes and finished title

--- LLC website/src/routes/+page.svelte
+++ LLC website/src/routes/+page.svelte
@@ -1,94 +1,166 @@
-<!-- Routes 👇 -->
-
-
 <script>
+  import { onMount } from "svelte";
+
+  /**
+   * @param {any} name
+   * @param {any} anim
+   */
+  function assignAnim(name, anim) {
+    let el = document.querySelector(name);
+    el.style.animation = anim;
+  }
 
 
-  import { EDITOR } from '$env/static/private';
- import { Router, Route, Link } from '../../node_modules/svelte-routing'
-  export let url = ""
- 
- 
-  import Education from './Education.svelte'
- </script>
- 
- 
- 
- 
- <main>
- 
- 
-  <!-- header -->
+
+  onMount(() => {
+    setTimeout(() => {
+      assignAnim(".paragraph", "fade 2s ease-in-out");
+
+      var i = 0;
+      var txt = "I'm a 14 Year old Sophomore at New Tech High School.";
+      var speed = 60;
+
+      function typeWriter() {
+        if (i < txt.length) {
+          let title = document.querySelector(".paragraph");
+
+          if (i == 30) {
+            // @ts-ignore
+            title.innerHTML += "<br>";
+          } else {
+            // @ts-ignore
+            title.innerHTML += txt.charAt(i);
+          }
+          i++;
+          setTimeout(typeWriter, speed);
+        }
+      }
+      typeWriter();
+    }, 1000);
+  });
+</script>
+
+<!-- Title -->
+<div>
+  <!-- svelte-ignore a11y-missing-content -->
+  <h1 class="title" >Hey, I'm <span class="gradient">Sachman</span>.</h1>
+  <p class="paragraph" />
+</div>
+
+<div id="Header">
   <header class="site-header">
     <div class="site-identity">
-      <h1><a href="#">Sachman's LLC Portfolio</a></h1>
+      <h1><a href="/">Sachman's LLC Portfolio</a></h1>
     </div>
     <nav class="site-navigation">
       <ul class="nav">
-        <li><a href="./Education">Education</a></li>
-        <li><a href="#">Family</a></li>
-        <li><a href="#">Hobbies</a></li>
+        <li><a href="/Education">Education</a></li>
+        <li><a href="/Family">Family</a></li>
+        <li><a href="/Hobbies">Hobbies</a></li>
       </ul>
     </nav>
+    <!-- create hamburger menu -->
+    <div class="menu">
+      <div class="line1" />
+      <div class="line2" />
+      <div class="line3" />
+    </div>
   </header>
- 
- 
- 
- 
-  <Router url={url}>
-    <nav class="site-navigation">
-      <ul class="nav">
-        <Link to="/Education">Education</Link>
-      </ul>
-    </nav>
- 
- 
-    <Route path="/Education"><Education /></Route>
-  </Router>
- 
- 
- 
- 
- 
- 
-  <!-- css -->
-  <style lang="scss">
-    body {
-      font-family: Helvetica;
-      margin: 0;
+</div>
+
+<style lang="scss">
+  @import url("https://fonts.googleapis.com/css2?family=Poppins");
+
+  #Header {
+    backdrop-filter: blur(10px);
+    position: fixed;
+    width: 100%;
+    margin-bottom: 1em;
+    top: 0;
+  }
+
+  .gradient {
+    background: linear-gradient(90deg, #9900ff, #1bb116);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: bold;
+  }
+
+  .paragraph {
+    font: 1.2em "Poppins", sans-serif;
+    color: #666464;
+    position: absolute;
+    left: 5.2%;
+    top: 42.2%;
+    padding: 0;
+  }
+
+  .menu {
+    display: none;
+    position: absolute;
+  }
+
+  body {
+    font-family: Helvetica;
+    margin: 0;
+  }
+  a {
+    text-decoration: none;
+    color: #000;
+    font-family: "Poppins", sans-serif;
+  }
+  .site-header {
+    border-bottom: 1px solid #ccc;
+    padding: 0.5em 1em;
+    display: flex;
+    justify-content: space-between;
+    font-family: "Poppins", sans-serif;
+  }
+
+  .site-identity h1 {
+    font-size: 1.5em;
+    margin: 0.6em 0;
+    display: inline-block;
+  }
+
+  .site-navigation ul,
+  .site-navigation li {
+    margin: 0;
+    padding: 0;
+  }
+
+  .site-navigation li {
+    display: inline-block;
+    margin: 1.4em 1em 1em 1em;
+  }
+
+  .title {
+    margin: 0.6em 0;
+    display: inline-block;
+    position: absolute;
+    top: 30%;
+    left: 5%;
+    font: 3em "Poppins", sans-serif, bold;
+    animation: fade 2s ease-in-out;
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 0;
     }
-    a {
-      text-decoration: none;
-      color: #000;
+    to {
+      opacity: 1;
     }
-    .site-header {
-      border-bottom: 1px solid #ccc;
-      padding: 0.5em 1em;
-      display: flex;
-      justify-content: space-between;
+  }
+
+  @media (max-width: 800px) {
+    .site-navigation {
+      display: none;
     }
- 
- 
-    .site-identity h1 {
-      font-size: 1.5em;
-      margin: 0.6em 0;
-      display: inline-block;
+    .menu {
+      display: block;
+      cursor: pointer;
     }
- 
- 
-    .site-navigation ul,
-    .site-navigation li {
-      margin: 0;
-      padding: 0;
-    }
- 
- 
-    .site-navigation li {
-      display: inline-block;
-      margin: 1.4em 1em 1em 1em;
-    }
-  </style>
- </main>
- 
- 
- 
+  }
+</style>
