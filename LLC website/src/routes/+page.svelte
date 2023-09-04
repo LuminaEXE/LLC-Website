0447b096b6c4a620814369c2040593a4ba0fc422
Lumina 3 <86614978+LuminaEXE@users.noreply.github.com>
2023-09-04 20:57:35 +0000

    e

--- LLC website/src/routes/+page.svelte
+++ LLC website/src/routes/+page.svelte
@@ -1,5 +1,8 @@
 <script>
+// @ts-nocheck
+
   import { onMount } from "svelte";
+  import Page from "./Education/+page.svelte";
 
   /**
    * @param {any} name
@@ -10,21 +13,19 @@
     el.style.animation = anim;
   }
 
-
-
   onMount(() => {
     setTimeout(() => {
       assignAnim(".paragraph", "fade 2s ease-in-out");
 
       var i = 0;
-      var txt = "I'm a 14 Year old Sophomore at New Tech High School.";
+      var txt = "I'm a Sophomore at New Tech High School @  Coppell.";
       var speed = 60;
 
       function typeWriter() {
         if (i < txt.length) {
           let title = document.querySelector(".paragraph");
 
-          if (i == 30) {
+          if (i == 27) {
             // @ts-ignore
             title.innerHTML += "<br>";
           } else {
@@ -36,20 +37,45 @@
         }
       }
       typeWriter();
-    }, 1000);
+    }, 500);
   });
 </script>
 
 <!-- Title -->
 <div>
   <!-- svelte-ignore a11y-missing-content -->
-  <h1 class="title" >Hey, I'm <span class="gradient">Sachman</span>.</h1>
+  <h1 class="title">Hey, I'm <span class="gradient">Sachman.</span></h1>
   <p class="paragraph" />
+  <button class="btnInf" on:click={
+    () => {
+      window.scrollTo({
+        left: 0,
+        top: 1000,
+        behavior: "smooth",
+      });
+    }}>More info!</button>
+</div>
+<div class="imgMe">
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <img
+    src="https://cdn.discordapp.com/attachments/1047367033215533087/1148333313593974865/IMG_1841.jpg"
+    width="800"
+    height="600"
+    class="imgM"
+  />
+</div>
+
+<div class="abtMe">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+  <h2 class="abtTitle" >About me</h2>
 </div>
 
 <div id="Header">
   <header class="site-header">
+    
     <div class="site-identity">
+      <img src="https://i.imgur.com/srWnyyy.png" width="60" height="60"/>
       <h1><a href="/">Sachman's LLC Portfolio</a></h1>
     </div>
     <nav class="site-navigation">
@@ -69,6 +95,63 @@
 </div>
 
 <style lang="scss">
+
+  .abtTitle {
+    font-family: "Poppins", sans-serif;
+    position: absolute;
+    top: 5%;
+    left: 5%;
+    font-size: 2.7em;
+    animation: fade-bottom 2s ease-in-out;
+  }
+
+  .abtMe {
+    position: absolute;
+    top: 100%;
+    left: 0%;
+    width: 90%;
+    height: 100%;
+    background-color: #ffffff;
+    animation: fade-bottom 2s ease-in-out;
+  }
+
+  .imgMe {
+    position: absolute;
+    top: 50%;
+    left: 75%;
+    transform: translate(-50%, -50%);
+  }
+  .imgM {
+    border-radius: 20px;
+    opacity: 25;
+    animation: fade-bottom 2s ease-in-out;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    overflow-x: hidden;
+  }
+  /* Firefox */
+  * {
+    scrollbar-width: auto;
+    scrollbar-color: #292929 #ffffff;
+  }
+
+  /* Chrome, Edge, and Safari */
+  *::-webkit-scrollbar {
+    width: 15px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: #ffffff;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: #292929;
+    border-radius: 8px;
+    border: 3px solid #ffffff;
+  }
   @import url("https://fonts.googleapis.com/css2?family=Poppins");
 
   #Header {
@@ -79,6 +162,13 @@
     top: 0;
   }
 
+  .footer {
+    width: 100%;
+    position: absolute;
+    bottom: 0;
+    height: 2.5rem;
+    background-color: rgb(98, 97, 97);
+  }
   .gradient {
     background: linear-gradient(90deg, #9900ff, #1bb116);
     -webkit-background-clip: text;
@@ -92,7 +182,7 @@
     color: #666464;
     position: absolute;
     left: 5.2%;
-    top: 42.2%;
+    top: 40.2%;
     padding: 0;
   }
 
@@ -122,12 +212,22 @@
     font-size: 1.5em;
     margin: 0.6em 0;
     display: inline-block;
+    align-items: center;
+    top: 50%;
+    transform: translateY(-40%);
+  }
+  .site-identity img {    
+    top: 50%;
+    transform: translateY(10%);
+    margin-right: 10px;
   }
 
   .site-navigation ul,
   .site-navigation li {
     margin: 0;
     padding: 0;
+    top: 0%;
+    transform: translateY(7.5%);
   }
 
   .site-navigation li {
@@ -142,7 +242,7 @@
     top: 30%;
     left: 5%;
     font: 3em "Poppins", sans-serif, bold;
-    animation: fade 2s ease-in-out;
+    animation: fade 0.5s ease-in-out;
   }
 
   @keyframes fade {
@@ -150,6 +250,14 @@
       opacity: 0;
     }
     to {
+      opacity: 1;
+    }
+  }
+  @keyframes fade-bottom {
+    0% {
+      opacity: 0;
+    }
+    100% {
       opacity: 1;
     }
   }
@@ -161,6 +269,75 @@
     .menu {
       display: block;
       cursor: pointer;
+    }
+    .paragraph {
+      top: 53.2%;
+    }
+  }
+
+  /* CSS */
+  .btnInf {
+    left: 5.2%;
+    top: 55.2%;
+
+    align-items: center;
+    appearance: none;
+    background-color: #fcfcfd;
+    border-radius: 4px;
+    border-width: 0;
+    // box-shadow: rgba(45, 35, 66, 0.4) 0 2px 4px,
+    //   rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+    box-sizing: border-box;
+    color: #36395a;
+    cursor: pointer;
+    display: inline-flex;
+    font-family: "Poppins", sans-serif;
+    height: 48px;
+    justify-content: center;
+    line-height: 1;
+    list-style: none;
+    overflow: hidden;
+    padding-left: 16px;
+    padding-right: 16px;
+    position: absolute;
+    text-align: left;
+    text-decoration: none;
+    transition: box-shadow 0.15s, transform 0.15s;
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    white-space: nowrap;
+    will-change: box-shadow, transform;
+    font-size: 18px;
+    font-weight: 300;
+    border: 1px solid #d6d6e7;
+    animation: fade-bottom 2s ease-in-out;
+  }
+
+
+  .btnInf:focus {
+    // box-shadow: #d6d6e7 0 0 0 1.5px inset, rgba(45, 35, 66, 0.4) 0 2px 4px,
+    //   rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+  }
+
+  .btnInf:hover {
+    box-shadow: rgba(45, 35, 66, 0.4) 0 4px 8px,
+      rgba(45, 35, 66, 0.3) 0 7px 13px -3px, #d6d6e7 0 -3px 0 inset;
+    transform: translateY(-2px);
+    background: linear-gradient(30deg, #ffffff, #f1ebf6);
+  }
+
+  .btnInf:active {
+    box-shadow: #d6d6e7 0 3px 7px inset;
+    transform: translateY(2px);
+  }
+
+  @keyframes def {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 0;
     }
   }
 </style>
--- LLC website/src/routes/Hobbies/+page.svelte
+++ LLC website/src/routes/Hobbies/+page.svelte
@@ -1,3 +1,21 @@
+<script>
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    let el = document.querySelector(".img1");
+    //@ts-ignore
+    el.addEventListener("animationend", () => {
+      //@ts-ignore
+      el.style.filter = "brightness(70%)";
+    });
+
+    el?.addEventListener("click", () => {
+      //make a drop down with text
+      
+    });
+  })
+</script>
+
 <div id="Header">
   <header class="site-header">
     <div class="site-identity">
@@ -13,8 +31,79 @@
   </header>
 </div>
 
+<h1 class="title gradient">Photography</h1>
+
+<img
+  src="https://i.imgur.com/WRCHnqx.jpg"
+  width="800px"
+  height="600px"
+  class="img1"
+/>
+<div class="felix-daddys-ballsack" />
+
+<h1 class="title gradient">Photography</h1>
+
+<img
+  src="https://i.imgur.com/WRCHnqx.jpg"
+  width="800px"
+  height="600px"
+  class=""
+/>
+<div class="" />
+
 <style lang="scss">
   @import url("https://fonts.googleapis.com/css2?family=Poppins");
+
+  .felix-daddys-ballsack {
+    background-image: linear-gradient(
+      90deg,
+      rgb(12, 52, 255),
+      rgb(255, 0, 170)
+    );
+    filter: blur(25px);
+    opacity: 0.5;
+    width: 810px;
+    height: 610px;
+    padding: 0;
+    margin: 0;
+    align-items: center;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 25%;
+    z-index: 0;
+  }
+
+  .title {
+    filter: drop-shadow(1px 1px 2rem rgb(255, 255, 255));
+    margin: 0.6em 0;
+    display: inline-block;
+    position: absolute;
+    font: 3em "Poppins", sans-serif, bold;
+    animation: fade 2s ease-in-out;
+    /* make centered */
+    left: 50%;
+    transform: translateX(-50%);
+    top: 10%;
+    font-weight: 900;
+  }
+  .gradient {
+    background: linear-gradient(40deg, #62ddf8, #ff7ad0);
+    -webkit-background-clip: text;
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: bold;
+  }
+  .img1 {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    top: 25.5%;
+    z-index: 1;
+  }
+  .img1:hover {
+    animation: tint-black 0.5s ease-in-out;
+  }
 
   #Header {
     backdrop-filter: blur(10px);
@@ -56,5 +145,21 @@
   .site-navigation li {
     display: inline-block;
     margin: 1.4em 1em 1em 1em;
+  }
+  @keyframes tint-black {
+    0% {
+      filter: brightness(100%);
+    }
+    100% {
+      filter: brightness(70%);
+    }
+  }
+  @keyframes tint-black-inv {
+    0% {
+      filter: brightness(70%);
+    }
+    100% {
+      filter: brightness(100%);
+    }
   }
 </style>
